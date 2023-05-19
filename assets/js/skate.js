b4e4c0089ff3eeed192aa7163283e776a971af33
emti <mawmawligm@gmail.com>
2023-05-19 19:31:24 +0330

    newversion1

--- assets/js/skate.js
+++ assets/js/skate.js
@@ -53,10 +53,11 @@ $(document).ready(function() {
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "150px";
-  document.getElementById("main").style.marginLeft = "150px";
+  document.getElementById("main").style.visibility = "hidden";
+
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("main").style.visibility= "visible";
 }
